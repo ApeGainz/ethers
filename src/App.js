c55ec35ba8529856a9e84034afad7c40ebf0264c
AkshitTheComputerScientist <akshit360@live.com>
2022-12-17 11:53:09 +0000

    added reading from an erc-721 functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import 'bulma/css/bulma.min.css';
+import Accounts from './Accounts';
+import ReadContract from './ReadContract';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>
+        Ethers.js example code
+        </h1>
+        <Accounts/>
+        <ReadContract/>
       </header>
     </div>
   );
